49382691a9d37da2cdcf05b8e134c5c01ba2c067
Eren AVAR <eren.avar@hotmail.com>
2024-07-23 00:45:32 +0300

    change background and add login screen

--- App.tsx
+++ App.tsx
@@ -1,17 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import LoginScreen from "./src/LoginScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ImageBackground
-        style={{ height: "100%", width: "100%" }}
-        source={require("./assets/cute-monkeys-orange-background.jpg")}
-        resizeMode="cover"
-      >
-        <LoginScreen />
-      </ImageBackground>
+      <LoginScreen />
     </View>
   );
 }
--- src/LoginScreen.tsx
+++ src/LoginScreen.tsx
@@ -1,8 +1,53 @@
 import React from "react";
-import { Text } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 const LoginScreen = () => {
-  return <Text>LoginScreen</Text>;
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.container}>
+        <Text style={styles.text}>Login</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="email"
+          placeholderTextColor="#999"
+        />
+        <TextInput style={styles.input} placeholder="password" />
+      </View>
+    </ImageBackground>
+  );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {},
+  text: {
+    fontSize: 50,
+  },
+  input: {
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 14,
+    width: 300,
+    marginVertical: 10,
+    backgroundColor: "white",
+    color: "red",
+  },
+});

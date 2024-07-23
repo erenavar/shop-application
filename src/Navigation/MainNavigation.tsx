d7b72abebefd4f44bedf40f2be190bbf775fba13
Eren AVAR <eren.avar@hotmail.com>
2024-07-23 11:40:29 +0300

    install native stack and bottom tab packages

---
+++ src/Navigation/MainNavigation.tsx
@@ -0,0 +1,17 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../LoginScreen";
+
+const Stack = createNativeStackNavigator();
+
+function MainNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainNavigation;

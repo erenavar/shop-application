29a81d61671786c807490ae34e34d55c8ead01ed
Eren AVAR <eren.avar@hotmail.com>
2024-07-30 14:55:19 +0300

    complete description area on details page

--- src/screens/Details/DetailsScreen.tsx
+++ src/screens/Details/DetailsScreen.tsx
@@ -5,6 +5,7 @@ import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { RootStackParamList, TabParamList } from "../../Navigation/types";
 import { StackScreenProps } from "@react-navigation/stack";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import GradientText from "../../components/GradientText";
 
 type Props = CompositeScreenProps<
   StackScreenProps<RootStackParamList, "Details">,
@@ -30,6 +31,13 @@ const DetailsScreen: FC<Props> = ({ route }) => {
           <Text style={styles.count}>(120)</Text>
         </View>
       </View>
+
+      <GradientText style={styles.descTitle} text="Description" />
+      <Text style={styles.descText}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores,
+        molestiae! Qui odit dignissimos tenetur aspernatur, nihil sequi
+        cupiditate unde aliquid, sint alias a nobis? Consequatur soluta
+      </Text>
     </View>
   );
 };
@@ -51,7 +59,10 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     marginTop: "3%",
   },
-  title: { fontSize: 18, fontFamily: "Helvetica" },
+  title: {
+    fontSize: 18,
+    fontFamily: "Helvetica",
+  },
   category: {
     fontSize: 16,
     color: "gray",
@@ -59,7 +70,10 @@ const styles = StyleSheet.create({
     fontFamily: "Times New Roman",
     fontWeight: "500",
   },
-  price: { fontSize: 18, fontFamily: "sans-serif" },
+  price: {
+    fontSize: 18,
+    fontFamily: "sans-serif",
+  },
   stars: {
     flexDirection: "row",
     justifyContent: "center",
@@ -69,4 +83,9 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "gray",
   },
+  descTitle: {
+    fontSize: 20,
+    marginVertical: "5%",
+  },
+  descText: { fontSize: 16, lineHeight: 20 },
 });

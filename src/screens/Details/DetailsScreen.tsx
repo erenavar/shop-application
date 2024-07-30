1aae417e5779fbc5c13fa64864a0144577f1b4a6
Eren AVAR <eren.avar@hotmail.com>
2024-07-30 14:03:14 +0300

    complete details page image css

--- src/screens/Details/DetailsScreen.tsx
+++ src/screens/Details/DetailsScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React, { FC } from "react";
 import { CompositeScreenProps } from "@react-navigation/native";
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
@@ -12,12 +12,39 @@ type Props = CompositeScreenProps<
 
 const DetailsScreen: FC<Props> = ({ route }) => {
   return (
-    <View>
-      <Text>{route.params.id}</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require("../../../assets/background.jpg")}
+      />
+      <View style={styles.info}>
+        <View>
+          <Text>1</Text>
+          <Text>2</Text>
+          <Text>3</Text>
+        </View>
+        <View>
+          <Text>4</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default DetailsScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: "2%",
+  },
+  image: {
+    height: "50%",
+    width: "100%",
+    borderRadius: 5,
+  },
+  info: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});

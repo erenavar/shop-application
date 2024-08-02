c56e6b29738496ef3fdee3ccb3d12845dc6bb526
Eren AVAR <eren.avar@hotmail.com>
2024-08-02 18:11:44 +0300

    recode all detail page css

--- src/screens/Details/DetailsScreen.tsx
+++ src/screens/Details/DetailsScreen.tsx
@@ -1,4 +1,11 @@
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React, { FC } from "react";
 import { CompositeScreenProps, useRoute } from "@react-navigation/native";
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
@@ -7,6 +14,7 @@ import { StackScreenProps } from "@react-navigation/stack";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import GradientText from "../../components/GradientText";
 import { useQuery } from "@tanstack/react-query";
+import Indicator from "../../components/Indicator";
 
 type Props = CompositeScreenProps<
   StackScreenProps<RootStackParamList, "Details">,
@@ -14,40 +22,40 @@ type Props = CompositeScreenProps<
 >;
 
 const DetailsScreen: FC<Props> = ({ route }) => {
-  const { data, error } = useQuery({
+  const { data, error, isLoading } = useQuery({
     queryKey: ["ProductDetails"],
-    queryFn: async () =>
+    queryFn: () =>
       fetch("https://fakestoreapi.com/products/" + `${route.params.id}`).then(
         (res) => res.json()
       ),
   });
-  console.log("data :>> ", data);
+  if (isLoading) return <Indicator />;
+  if (error) console.log("error :>> ", error);
 
   return (
     <View style={styles.container}>
-      <Text>{route.params.id}</Text>
-      <Image
-        style={styles.image}
-        source={require("../../../assets/background.jpg")}
-      />
-      <View style={styles.info}>
-        <View>
-          <Text style={styles.title}>Body Suit</Text>
-          <Text style={styles.category}>Mother Care</Text>
-          <Text style={styles.price}>$400</Text>
+      <ScrollView>
+        <Image
+          style={styles.image}
+          source={{ uri: data?.image }}
+          resizeMode="contain"
+        />
+        <View style={styles.info}>
+          <View style={styles.firstColumn}>
+            <Text style={styles.title}>{data.title}</Text>
+            <Text style={styles.category}>{data.category}</Text>
+            <Text style={{ fontSize: 16 }}>${data.price}</Text>
+          </View>
+          <View style={styles.stars}>
+            <FontAwesome name="star" size={18} color="orange" />
+            <Text style={{ fontSize: 16 }}>{data.rating.rate}</Text>
+            <Text style={styles.count}>({data.rating.count})</Text>
+          </View>
         </View>
-        <View style={styles.stars}>
-          <FontAwesome name="star" size={18} color="orange" />
-          <Text style={{ fontSize: 16 }}>4.9</Text>
-          <Text style={styles.count}>(120)</Text>
-        </View>
-      </View>
-      <GradientText style={styles.descTitle} text="Description" />
-      <Text style={styles.descText}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores,
-        molestiae! Qui odit dignissimos tenetur aspernatur, nihil sequi
-        cupiditate unde aliquid, sint alias a nobis? Consequatur soluta
-      </Text>
+        <GradientText style={styles.descTitle} text="Description" />
+        <Text style={{ flex: 1 }}>{data.description}</Text>
+      </ScrollView>
+
       <View style={styles.bottom}>
         <Pressable style={styles.heart}>
           <FontAwesome name="heart-o" size={24} color="rebeccapurple" />
@@ -67,21 +75,22 @@ export default DetailsScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: "100%",
+    height: "100%",
     padding: "5%",
   },
-  image: {
-    height: "47%",
-    width: "100%",
-    borderRadius: 5,
-  },
+  scroll: { paddingBottom: 60 },
+  image: { height: "100%", width: "100%", borderRadius: 5 },
   info: {
     flexDirection: "row",
     justifyContent: "space-between",
-    marginTop: "3%",
+    marginVertical: "3%",
+  },
+  firstColumn: {
+    flex: 7,
   },
   title: {
-    fontSize: 18,
+    fontSize: 16,
     fontFamily: "Helvetica",
   },
   category: {
@@ -91,19 +100,13 @@ const styles = StyleSheet.create({
     fontFamily: "Times New Roman",
     fontWeight: "500",
   },
-  price: {
-    fontSize: 18,
-    fontFamily: "sans-serif",
-  },
   stars: {
     flexDirection: "row",
     justifyContent: "center",
     gap: 3,
+    flex: 3,
   },
-  count: {
-    fontSize: 16,
-    color: "gray",
-  },
+  count: { fontSize: 16, color: "gray" },
   descTitle: {
     fontSize: 20,
     marginVertical: "5%",

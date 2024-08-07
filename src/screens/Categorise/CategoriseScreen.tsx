e01a310afc1815b8f214ec4c79cdc7a8dbcdbb3b
Eren AVAR <eren.avar@hotmail.com>
2024-08-07 23:00:15 +0300

    fetch data from api to categorise screen

--- src/screens/Categorise/CategoriseScreen.tsx
+++ src/screens/Categorise/CategoriseScreen.tsx
@@ -4,6 +4,7 @@ import { CompositeScreenProps } from "@react-navigation/native";
 import { RootStackParamList, TabParamList } from "../../Navigation/types";
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { StackScreenProps } from "@react-navigation/stack";
+import { useQuery } from "@tanstack/react-query";
 
 type Props = CompositeScreenProps<
   StackScreenProps<RootStackParamList, "Categorise">,
@@ -11,6 +12,16 @@ type Props = CompositeScreenProps<
 >;
 
 const CategoriseScreen: FC<Props> = () => {
+  const { data } = useQuery({
+    queryKey: ["categorised"],
+    queryFn: () =>
+      fetch("https://fakestoreapi.com/products/category/jewelery").then((res) =>
+        res.json()
+      ),
+  });
+
+  console.log("data :>> ", data);
+
   return (
     <View>
       <Text>CategoriseScreen</Text>

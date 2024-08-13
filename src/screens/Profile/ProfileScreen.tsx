26122c1b694a5e38774759a544f2662472574455
Eren AVAR <eren.avar@hotmail.com>
2024-08-13 16:32:59 +0300

    get islogin to profile screen

--- src/screens/Profile/ProfileScreen.tsx
+++ src/screens/Profile/ProfileScreen.tsx
@@ -1,7 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const ProfileScreen = () => {
+  useEffect(() => {
+    AsyncStorage.getItem("isLogin").then((value: string | null) => {
+      console.log("value :>> ", value);
+    });
+  }, []);
+
   return (
     <View>
       <Text>ProfileScreen</Text>
